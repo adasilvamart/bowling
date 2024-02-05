62d6ec1abb889ed094c94bb1ac46db3967507054
adasilvamart <adasilva.clase@gmail.com>
2024-02-05 10:01:51 +0100

    refactor(src): :recycle: Refactor method names

--- src/game.py
+++ src/game.py
@@ -1,98 +1,94 @@
 class Game:
     VALID_CHARS = "123456789/x-"
-    STRIKE = 'x'
-    SPARE = '/'
-    ZERO = '-'
+    STRIKE = "x"
+    SPARE = "/"
+    ZERO = "-"
 
-    def __init__(self, pins):     
-        self.pins = self.validatePins(pins)
-        self.numPins = self.setNumPins()
+    def __init__(self, pins):
+        self.pins = self.is_pin_valid(pins)
+        self.num_pins = self.calc_pin_values()
 
+        self.score = self.calculate_score()[0]
+        self.frame_score = self.calculate_score()[1]
+
+    def get_score(self):
+        return self.score
 
     def __repr__(self):
-        return f'{self.pins}'
-    
+        return f"{self.pins}"
 
-    def getPins(self):
+    def get_pins(self):
         return self.pins
 
+    def get_num_pins(self):
+        return self.num_pins
 
-    def getNumPins(self):
-        return self.numPins
-
-
-    def setNumPins(self):
+    def calc_pin_values(self):
         """
-            Translate Pins into his numeric values
+        Translate Pins into their numeric values
         """
-        pins = self.getPins()
-        numPins=[]
+        pins = self.get_pins()
+        num_pins = []
 
         for i, pin in enumerate(pins):
 
-            if pin.isdigit(): # Pin
-                numPins.append(int(pin))
+            if pin.isdigit():  # Pin
+                num_pins.append(int(pin))
 
-            elif pin in self.SPARE: # Spare 
+            elif pin in self.SPARE:  # Spare
                 if pins[i - 1] in self.ZERO:
-                    numPins.append(10)
-                else:    
-                    numPins.append(10 - int(pins[i - 1]))
+                    num_pins.append(10)
+                else:
+                    num_pins.append(10 - int(pins[i - 1]))
 
-            elif pin in self.ZERO: # Nill
-                numPins.append(0)
+            elif pin in self.ZERO:  # Nill
+                num_pins.append(0)
 
-            else: # Strike
-                numPins.append(10)
+            else:  # Strike
+                num_pins.append(10)
 
-        return numPins   
+        return num_pins
 
-
-    def validatePins(self, pins):
+    def is_pin_valid(self, pins):
         if all(pin.lower() in self.VALID_CHARS for pin in pins):
             return pins
         raise ValueError
- 
 
-    def calcScore(self):
+    def calculate_score(self):
         score = 0
-        frameScore = []
+        frame_score = []
 
-        numPins = self.getNumPins()
-        frames = self.getPins()
-        
+        num_pins = self.get_num_pins()
+        frames = self.get_pins()
+
         i = 0
-        for frame in range(0, 10): 
-            if self.isStrike(numPins, frames, i):
-                frameScore.append(score + self.calcStrike(numPins, i))
-                score += self.calcStrike(numPins, i)
+        for frame in range(0, 10):
+            if self.is_strike(num_pins, frames, i):
+                frame_score.append(score + self.calculate_strike(num_pins, i))
+                score += self.calculate_strike(num_pins, i)
                 i += 1
 
-            elif self.isSpare(numPins, frames, i):
-                frameScore.append(score + self.calcSpare(numPins, i))
-                score += self.calcSpare(numPins, i)
-                
+            elif self.is_spare(num_pins, frames, i):
+                frame_score.append(score + self.calculate_spare(num_pins, i))
+                score += self.calculate_spare(num_pins, i)
+
                 i += 2
 
             else:
-                frameScore.append(score + numPins[i] + numPins[i + 1])
-                score += numPins[i] + numPins[i + 1]
+                frame_score.append(score + num_pins[i] + num_pins[i + 1])
+                score += num_pins[i] + num_pins[i + 1]
                 i += 2
 
-        return score, frameScore
-        
+        return score, frame_score
 
-    def calcSpare(self, numPins, i):
-        return 10 + numPins[i + 2]
+    def calculate_spare(self, num_pins, i):
+        return 10 + num_pins[i + 2]
 
+    def calculate_strike(self, num_pins, i):
+        return 10 + num_pins[i + 1] + num_pins[i + 2]
 
-    def calcStrike(self, numPins, i):
-        return 10 + numPins[i + 1] + numPins[i + 2]
+    def is_spare(self, num_pins, frames, i):
+        return num_pins[i] + num_pins[i + 1] == 10 and self.SPARE in frames[i + 1]
 
-
-    def isSpare(self, numPins, frames, i):
-        return numPins[i] + numPins[i + 1] == 10 and self.SPARE in frames[i + 1]
-
-
-    def isStrike(self, numPins, frames, i):
-        return frames[i].lower() == self.STRIKE and numPins[i] == 10
+    def is_strike(self, num_pins, frames, i):
+        return frames[i].lower() == self.STRIKE and num_pins[i] == 10
--- src/scorecard.py
+++ src/scorecard.py
@@ -1,90 +1,84 @@
 from src.game import Game
 
-class ScoreCard():
+
+class ScoreCard:
     def __init__(self, pins) -> None:
         self.game = Game(pins)
-        self.score = self.game.calcScore()
-  
+        self.score = self.game.calculate_score()
 
     def __repr__(self):
-        return self.formatScorecard()
+        return self.format_scorecard()
 
-
-    def getScore(self):
+    def get_score(self):
         return self.score[0]
-    
 
-    def formatScorecard(self):
-        frames, totalPoints = self.getFramesScore()
+    def format_scorecard(self):
+        frames, totalPoints = self.get_frames_score()
         formatted_frame = "|"
         formatted_result = "|"
         middle_line = "|"
         total = f" Total Points: {totalPoints}"
 
         for frame, result in frames:
-            
+
             if len(frame) == 3:
-                formatted_frame += f' {frame[0]} | {frame[1]} | {frame[-1]} |'
+                formatted_frame += f" {frame[0]} | {frame[1]} | {frame[-1]} |"
                 middle_line += "   --------|"
-                formatted_result += f'    {result}    |'
+                formatted_result += f"    {result}    |"
             else:
                 if frame == self.game.STRIKE:
-                    formatted_frame += f' - | {frame[0]} |'    
+                    formatted_frame += f" - | {frame[0]} |"
 
-                formatted_frame += f' {frame[0]} | {frame[-1]} |'
+                formatted_frame += f" {frame[0]} | {frame[-1]} |"
 
                 if len(str(result)) == 3:
-                    formatted_result += f'  {result}  |'
+                    formatted_result += f"  {result}  |"
                 elif len(str(result)) == 1:
-                    formatted_result += f'   {result}   |'
+                    formatted_result += f"   {result}   |"
                 else:
-                    formatted_result += f'   {result}  |'
-            
-                middle_line += "   ----|"      
-        
-        return str(f'{formatted_frame}\n{middle_line}{total}\n{formatted_result}')
+                    formatted_result += f"   {result}  |"
 
+                middle_line += "   ----|"
 
-    def getFramesScore(self):
-        totalScore, frameScore = self.game.calcScore()        
-        frames = self.buildFrames(self.game.getPins())   
-        return list(zip(frames, frameScore)), totalScore
+        return str(f"{formatted_frame}\n{middle_line}{total}\n{formatted_result}")
 
+    def get_frames_score(self):
+        total_score, frame_score = self.game.calculate_score()
+        frames = self.build_frames(self.game.get_pins())
+        return list(zip(frames, frame_score)), total_score
 
     def padded_pins(self, pins) -> str:
-        padded_pins = ''
-        if not self.hasThreeRolls():
+        padded_pins = ""
+        if not self.has_three_rolls():
             for i in range(0, len(pins)):
-                if pins[i].lower() != 'x':
+                if pins[i].lower() != "x":
                     padded_pins += pins[i]
                 else:
-                    padded_pins += '-' + pins[i]
+                    padded_pins += "-" + pins[i]
             return padded_pins
-        
+
         else:
             for i in range(0, len(pins[:-3])):
-                if pins[i].lower() != 'x':
+                if pins[i].lower() != "x":
                     padded_pins += pins[i]
                 else:
-                    padded_pins += '-' + pins[i]
+                    padded_pins += "-" + pins[i]
             return padded_pins + pins[-3:]
 
-
-    def buildFrames(self, pins):
+    def build_frames(self, pins):
         padded_pins = self.padded_pins(pins)
         frames = []
-        if self.hasThreeRolls():
+        if self.has_three_rolls():
             for i in range(0, len(padded_pins[:-3]), 2):
-                frames.append(padded_pins[i:i+2])
+                frames.append(padded_pins[i : i + 2])
             frames.append(padded_pins[-3:])
         else:
             for i in range(0, len(padded_pins), 2):
-                frames.append(padded_pins[i:i+2])
+                frames.append(padded_pins[i : i + 2])
         return frames
 
-            
-    def hasThreeRolls(self):
-        pins = self.game.getPins()
+    def has_three_rolls(self):
+        pins = self.game.get_pins()
         if any(pin.lower() == "x" or pin == "/" for pin in pins[-3:]):
             return True
         return False
